2eda6b8e6f517bb48c53812f001f9566c46c2b74
Kien <iniesta113@gmail.com>
2016-11-19 21:04:18 +0700

    test sT

--- app/src/main/java/com/example/kien/testmusic/Model/TestSourceTree.java
+++ app/src/main/java/com/example/kien/testmusic/Model/TestSourceTree.java
@@ -6,4 +6,16 @@ package com.example.kien.testmusic.Model;
 
 public class TestSourceTree {
     private String test;
+
+    public TestSourceTree(String test) {
+        this.test = test;
+    }
+
+    public String getTest() {
+        return test;
+    }
+
+    public void setTest(String test) {
+        this.test = test;
+    }
 }
